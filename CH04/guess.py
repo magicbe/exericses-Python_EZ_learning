34c93a8d86f20d6927060814be938a12774c1501
magicbe <magicbe.tw@gmail.com>
2019-02-15 17:40:38 +0800

    update CH04 exercises - guess number (final version)

--- CH04/guess.py
+++ CH04/guess.py
@@ -5,14 +5,22 @@
 
 import random
 
-r = random.randint(1, 100)
+start = int(input('請決定隨機數字範圍開始值：'))
+end = int(input('請決定隨機數字範圍結束值：'))
+
+r = random.randint(start, end)
+count = 0
+
 while True:
+    count += 1 # count = count + 1
     num = input('請猜數字：')
     num = int(num)
     if num == r:
         print('你猜中了！')
+        print('這是你猜的第', count, '次')
         break
     elif num > r:
         print('比答案大')
     elif num < r:
         print('比答案小')
+    print('這是你猜的第', count, '次')
