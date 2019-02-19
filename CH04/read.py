f4db4d45ce76c7b8f54753b5a8ee4ee49a800f8e
Magicbe <magicbe.tw@gmail.com>
2019-02-19 22:28:12 +0800

    update read

--- CH04/read.py
+++ CH04/read.py
@@ -14,17 +14,19 @@ for d in data:
 
 print('留言的平均長度為', sum_len/len(data))
 
-new = []
-for d in data:
-    if len(d) < 100:
-        new.append(d)
+# new = []
+# for d in data:
+#     if len(d) < 100:
+#         new.append(d)
+new = [d for d in data if len(d) < 100]
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
-good = []
-for d in data:
-    if 'good' in d:
-        good.append(d)
+# good = []
+# for d in data:
+#     if 'good' in d:
+#         good.append(d)
+good = [d for d in data if 'good' in d]
 print('一共有', len(good), '筆留言提到good')
 print(good[0])
