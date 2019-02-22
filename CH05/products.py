7e3eeb3325ef860b3fdfde5e7ef2123c6b9f144e
magicbe <magicbe.tw@gmail.com>
2019-02-22 12:25:33 +0800

    update products - isfile

--- CH05/products.py
+++ CH05/products.py
@@ -1,12 +1,19 @@
+import os # operating system
+
 products = []
-# 讀取檔案
-with open('products.csv', 'r', encoding='UTF-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue # 繼續
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+    print('yeah! 找到檔案了！')
+    # 讀取檔案
+    with open('products.csv', 'r', encoding='UTF-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue # 繼續
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案......')
 
 # 讓使用者輸入
 while True:
