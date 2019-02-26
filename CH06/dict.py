1e723f94dec5b2726ac79c05abe4e41d3bb931d2
magicbe <magicbe.tw@gmail.com>
2019-02-27 00:12:47 +0800

    update dict

---
+++ CH06/dict.py
@@ -0,0 +1,12 @@
+# dictionary 字典
+
+words = {
+    'ramen': '拉麵',
+    'pasta': '義大利麵'
+}
+
+words['tea'] = '茶' # 增加新的key
+
+print(words)
+# print(words['ramen'])
+# print(words['pasta'])
---
+++ CH06/dict0.py
@@ -0,0 +1,13 @@
+p0 = {
+    'name': '麥香奶茶',
+    'price': 10
+}
+
+p1 = {
+    'name': '珍珠奶茶',
+    'price': 60
+}
+
+drinks = [p0, p1]
+print(drinks[0]['name']) # 麥香奶茶
+print(drinks[1]['price']) # 60
