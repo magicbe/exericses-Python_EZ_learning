0e1cd12454541d159b1f54f25396ccee9007bb83
magicbe <magicbe.tw@gmail.com>
2019-02-28 00:22:37 +0800

    update Image files convert

--- CH07/img.py
+++ CH07/img.py
@@ -1,4 +1,8 @@
 from PIL import Image
-image_file = Image.open("convert_image.png") # open colour image
-image_file = image_file.convert('L') # convert image to black and white
-image_file.save('result.png')
+import os
+
+for file in os.listdir('.'):
+    if file.endswith('.jpg'):
+        image_file = Image.open(file)  # open colour image
+        image_file = image_file.convert('L')  # convert image to black and white
+        image_file.save(file[:-4] + '_grey.png')
